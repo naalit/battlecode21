bf2a3b27fed7713c29e68378ae9cb51ae1a902e4
tolziplohu <sam@dirkback.com>
2021-01-11 17:35:59 +0000

    Make test compile, gradle update

--- .gitpod.dockerfile
+++ .gitpod.dockerfile
@@ -1,4 +1,6 @@
 FROM gitpod/workspace-full
 
 RUN sudo apt-get update \
- && sudo apt-get install -y openjdk-8-jdk
+ && sudo apt-get install -y openjdk-8-jdk \
+ && export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64 \
+ && export PATH=$JAVA_HOME/bin:$PATH
--- test/testplayer/FlagTest.java
+++ test/testplayer/FlagTest.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 public class FlagTest {
 	@Test
 	public void testConversion() {
-    Flag one = new Flag(new MapLocation(8234, 3245), Flag.Type.Edge, new MapLocation(8259, 3197));
-    Flag two = new Flag(new MapLocation(8234, 3245), one.encode());
+    Flag one = new Flag(new MapLocation(8234, 3245), Flag.Type.Edge, new MapLocation(8259, 3197), 0);
+    Flag two = new Flag(new MapLocation(8234, 3245), 0, one.encode(0));
     assertEquals(
       "one: " + one + ", two: " + two,
-      two.encode(),
-      one.encode()
+      two.encode(0),
+      one.encode(0)
     );
 
     // TODO add more tests
